9ba1e45b8d8b95d065f2544dfed9dccb398be911
Daniel Chyan <daniel@pkcsecurity.com>
2018-10-26 16:24:04 -0700

    grab device info

--- src/t/clj/routes.clj
+++ src/t/clj/routes.clj
@@ -1,7 +1,8 @@
 (ns t.clj.routes
   (:require [t.clj.routes.impl :as impl]
             [t.cljc.routes :as cljc-routes]
-            [ring.middleware.resource :as resource]))
+            [ring.middleware.resource :as resource]
+            [ring.middleware.params :as params]))
 
 (def handler 
   (impl/init-handler cljc-routes/routes))
@@ -9,4 +10,5 @@
 (def app
   (-> #'handler
       (impl/wrap-reload)
+      (params/wrap-params)
       (resource/wrap-resource "public")))
